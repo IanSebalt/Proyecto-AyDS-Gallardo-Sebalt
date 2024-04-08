e3a368ce761746cfb33ca369df2520f1347c30f3
Ian Sebalt <iansebalt@live.com.ar>
2024-04-07 23:11:54 -0300

    Factorización del conversor de fechas.

--- app/src/main/java/ayds/songinfo/utils/UtilsInjector.kt
+++ app/src/main/java/ayds/songinfo/utils/UtilsInjector.kt
@@ -4,6 +4,8 @@ import ayds.songinfo.utils.navigation.NavigationUtils
 import ayds.songinfo.utils.navigation.NavigationUtilsImpl
 import ayds.songinfo.utils.view.ImageLoader
 import ayds.songinfo.utils.view.ImageLoaderImpl
+import ayds.songinfo.utils.view.DateConversor
+import ayds.songinfo.utils.view.DateConversorImpl
 import com.squareup.picasso.Picasso
 
 object UtilsInjector {
@@ -11,4 +13,6 @@ object UtilsInjector {
     val imageLoader: ImageLoader = ImageLoaderImpl(Picasso.get())
 
     val navigationUtils: NavigationUtils = NavigationUtilsImpl()
+
+    val dateConversor: DateConversor = DateConversorImpl()
 }
